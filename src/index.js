b5d47c539c2dd31761fccfab7367febab9704afb
Fengda HUANG <h@phodal.com>
2016-06-21 07:20:52 +0800

    add basic functionnal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-export function add(x, y) {
-  return x + y;
+export function dlm() {
+
 }
+
+dlm.prototype.get = function (array) {
+
+};
+
+dlm.prototype.from = function (originObject) {
+
+};
+
+dlm.prototype.to = function (newObject) {
+
+};
+
+dlm.prototype.handle = function (field, callback) {
+
+};
+
+dlm.prototype.add = function (field, value) {
+
+};
+
+dlm.prototype.remove = function (field) {
+
+};
